eafaa25ed5fa1efdd928918819d936f4e76478c2
Marco Casu <mcasu299@gmail.com>
2024-05-30 18:54:07 +0200

    30-05-2024

--- Secondo Anno/Secondo Semestre/Basi di Dati 2/SQL/Accademia 7.sql
+++ Secondo Anno/Secondo Semestre/Basi di Dati 2/SQL/Accademia 7.sql
@@ -1,8 +1,13 @@
 Domanda 1)
 
+SELECT posizione, avg(stipendio) as media, 
+	stddev(stipendio) as deviazione_standard
+FROM Persona
+GROUP BY posizione;
 --------------------------------------------------
 
 Domanda 2)
+
 SELECT distinct *
 FROM Persona per
 WHERE per.stipendio > (SELECT avg(stipendio)
@@ -12,13 +17,26 @@ WHERE per.stipendio > (SELECT avg(stipendio)
 
 Domanda 3)
 
+SELECT per2.posizione,count(per2.id) 
+	FROM Persona per2, (SELECT posizione, avg(stipendio) as media, 
+	stddev(stipendio) as deviazione_standard
+	FROM Persona
+	GROUP BY posizione) Q
+WHERE Q.posizione=per2.posizione
+AND ABS(per2.stipendio-media)<=deviazione_standard
+GROUP BY per2.posizione;
 --------------------------------------------------
 
 Domanda 4)
 
+SELECT * FROM (SELECT per.id, per.nome, per.cognome, sum(oredurata) as ore
+			FROM Persona per join Attivitaprogetto att on att.persona=per.id
+			GROUP BY per.id, per.nome, per.cognome)
+WHERE ore>=20;
 --------------------------------------------------
 
 Domanda 5)
+
 SELECT nome,(fine-inizio) as durata
 FROM Progetto WHERE (fine-inizio)>(
 SELECT avg(fine-inizio) as durata
@@ -26,6 +44,7 @@ FROM Progetto pr);
 --------------------------------------------------
 
 Domanda 6)
+
 SELECT nome,sum(oredurata) as durata_complessiva
 FROM Progetto pr  join attivitaprogetto att on att.progetto=pr.id
 WHERE fine<NOW()
@@ -35,4 +54,18 @@ GROUP BY nome;
 
 Domanda 7)
 
+SELECT * FROM 
+	(SELECT per.id, per.nome, per.cognome, count(distinct ass.id) as assenze
+	FROM Persona per join assenza ass on ass.persona=per.id
+	WHERE posizione='Professore Ordinario'
+	AND ass.tipo='Malattia'
+	GROUP BY per.id, per.nome, per.cognome)
+WHERE assenze > (
+	SELECT avg(assenze2) FROM 
+	(SELECT per.id, per.nome, per.cognome, count(distinct ass.id) as assenze2
+	FROM Persona per join assenza ass on ass.persona=per.id
+	WHERE posizione='Professore Associato'
+	AND ass.tipo='Malattia'
+	GROUP BY per.id, per.nome, per.cognome)
+)
 --------------------------------------------------
--- Secondo Anno/Secondo Semestre/Reti di Elaboratori/Esercizi/readme.md
+++ Secondo Anno/Secondo Semestre/Reti di Elaboratori/Esercizi/readme.md
@@ -1,4 +1,8 @@
 # Esercizi di Reti degli Elaboratori
+
+---
+
+## Esonero 1
 - [Esercizi per il primo esonero](https://github.com/CasuFrost/University_notes/blob/main/Secondo%20Anno/Secondo%20Semestre/Reti%20di%20Elaboratori/Esercizi/Esonero1/Esercizi%20Esonero%201.pdf)
 - [Esercitazione sul livello di trasporto](https://github.com/CasuFrost/University_notes/blob/main/Secondo%20Anno/Secondo%20Semestre/Reti%20di%20Elaboratori/Esercizi/Esonero1/Esercitazione%20livello%20di%20Trasporto.pdf)
 - [Esonero di aprile 2015](https://github.com/CasuFrost/University_notes/blob/main/Secondo%20Anno/Secondo%20Semestre/Reti%20di%20Elaboratori/Esercizi/Esonero1/Esonero%20aprile%202015.pdf)
@@ -6,4 +10,10 @@
 - [Esame del 7 giugno 2022](https://github.com/CasuFrost/University_notes/blob/main/Secondo%20Anno/Secondo%20Semestre/Reti%20di%20Elaboratori/Esercizi/Esonero1/Esame%207%20giugno%202022.pdf)
 - [Esonero del 31 marzo  2023](https://github.com/CasuFrost/University_notes/blob/main/Secondo%20Anno/Secondo%20Semestre/Reti%20di%20Elaboratori/Esercizi/Esonero1/Esonero%2031%20marzo%202023.pdf)
 - [Esame del 7 settembre 2023](https://github.com/CasuFrost/University_notes/blob/main/Secondo%20Anno/Secondo%20Semestre/Reti%20di%20Elaboratori/Esercizi/Esonero1/Esame%207%20settembre%202023.pdf)
+
+## Esonero 2
+- [Esame del 11 aprile 2016](https://github.com/CasuFrost/University_notes/blob/main/Secondo%20Anno/Secondo%20Semestre/Reti%20di%20Elaboratori/Esercizi/Esonero2/Esame%2011%20aprile%202016.pdf)
+- [Esame del 8 giugno 2018](https://github.com/CasuFrost/University_notes/blob/main/Secondo%20Anno/Secondo%20Semestre/Reti%20di%20Elaboratori/Esercizi/Esonero2/Esame%208%20giugno%202018.pdf)
+- [Esame del 7 giugno 2022](https://github.com/CasuFrost/University_notes/blob/main/Secondo%20Anno/Secondo%20Semestre/Reti%20di%20Elaboratori/Esercizi/Esonero2/Esame%207%20giugno%202022.pdf)
+
 
